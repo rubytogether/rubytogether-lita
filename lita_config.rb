885e8726ccea4457aecd19b888f222d576974881
Andre Arko <andre@arko.net>
2016-08-03 00:19:37 -0700

    add redis and http port for local dev, too

--- lita_config.rb
+++ lita_config.rb
@@ -12,10 +12,10 @@ Lita.configure do |config|
       "indirect" => "U03LDE805",
       "cyrin" => "U0ZBFPJD9"
     }.values
-
-    config.redis[:url] = ENV["REDISTOGO_URL"]
-    config.http.port = ENV.fetch("PORT", 13374)
   else
     config.robot.adapter = :shell
   end
+
+  config.redis[:url] = ENV["REDISTOGO_URL"] || ENV["REDIS_URL"] || "redis://localhost:6379"
+  config.http.port = ENV["PORT"] || "13374"
 end
